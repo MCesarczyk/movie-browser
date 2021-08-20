13ec8b33ce666025463f7109b7a9a419287b1871
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-21 00:21:10 +0200

    removed line spacings

--- src/features/Navigation/index.js
+++ src/features/Navigation/index.js
@@ -4,14 +4,17 @@ import { StyledNavLink, Item, List } from "./styled"
 export const Navigation = () => {
     return (
         <nav>
-         
             <List>
             <Header />
                 <Item>
-                    <StyledNavLink to="/movies">Movies</StyledNavLink>
+                    <StyledNavLink to="/movies">
+                        Movies
+                    </StyledNavLink>
                 </Item>
                 <Item>
-                    <StyledNavLink to="/people">People</StyledNavLink>
+                    <StyledNavLink to="/people">
+                        People
+                    </StyledNavLink>
                 </Item>
             </List>
         </nav>
