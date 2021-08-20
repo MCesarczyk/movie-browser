f35633995d25a0d6cafa8c0c7f33d831e7a3b1dc
Kamil Stawik <kamil_stawik@op.pl>
2021-08-21 00:15:12 +0200

    Wrapper added to MoviePage feature

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Tile from "../../../common/Tile"
+import { Wrapper } from "./styled";
 
 const MoviePage = () => {
 
     return (
-        <div>Movie Page</div>
+        <>
+            <div>Movie Page</div>
+            <Wrapper>
+                <Tile />
+            </Wrapper>
+        </>
     );
 };
 
