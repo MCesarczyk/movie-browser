286c1f17be74f98e7f0ef83ca7596ad896ae8dea
Kamil Stawik <kamil_stawik@op.pl>
2021-08-20 23:39:53 +0200

    Poppins font added to index.html and body

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,11 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-html {
+  html {
     box-sizing: border-box;
   }
-  
+
+  body {
+    font-family: "Poppins", sans-serif;
+  }
+    
   *, ::after, ::before {
     box-sizing: inherit;
   }
-  `;
+`;
