a7961b90c4a8342401d0d24202789aa8abaf0551
Kamil Stawik <kamil_stawik@op.pl>
2021-08-20 22:26:28 +0200

    mystic color added to theme.js

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,7 @@ export const theme = {
         mercury: "#E5E5E5",
         stormGrey: "#74788B",
         woodsmoke: "#18181B",
+        mystic: "#E4E6F0",
     },
     breakpoint: {
         mobile: "767px",
