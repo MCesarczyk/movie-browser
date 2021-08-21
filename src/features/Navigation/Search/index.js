fd24ac82692bb16de41d257035e32a6bafe8480b
Justyna Pawlik <juspaw11@wp.pl>
2021-08-21 11:59:04 +0200

    added styles for input

--- src/features/Navigation/Search/index.js
+++ src/features/Navigation/Search/index.js
@@ -2,7 +2,6 @@ import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 import search from "../images/search.svg";
 
 export const Search = () => {
-
     return (
         <SearchWrapper>
             <SearchIcon src={search} alt="" />
--- src/features/Navigation/Search/styled.js
+++ src/features/Navigation/Search/styled.js
@@ -4,9 +4,10 @@ export const SearchIcon = styled.img`
 `;
 
 export const SearchWrapper = styled.div`
-
 `;
 
-export const SearchInput = styled.input`  
-placeholder="Search for movies.."
+export const SearchInput = styled.input` 
+height: 48px;
+width: 432px;
+border-radius: 33px;
 `;
