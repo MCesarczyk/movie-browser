bc29aa2272de92efcd65e30e9e32c3adf5ea907f
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-23 01:07:49 +0200

    temporarily resigned from Saga

--- src/rootSaga.js
+++
@@ -1,9 +0,0 @@
-import { all } from "redux-saga/effects";
-import { moviesSaga } from "./features/movies/moviesSaga";
-import { peopleSaga } from "./features/movies/peopleSaga";
-
-export default function* rootSaga() {
-    yield all([
-        moviesSaga(),
-    ]);
-}
