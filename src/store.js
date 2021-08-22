df4a65b3aa17db9d89e310e03c05b99325d2b65d
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-23 01:09:30 +0200

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,17 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import createSagaMiddleware from "@redux-saga/core";
 import moviesReducer from "./features/movies/moviesSlice";
-import { moviesSaga } from "./features/movies/moviesSaga";
-
-const sagaMiddleware = createSagaMiddleware();
-
 const store = configureStore({
     reducer: {
         movies: moviesReducer,
     },
-    middleware: [sagaMiddleware],
 });
-
-sagaMiddleware.run(moviesSaga);
 
 export default store;
