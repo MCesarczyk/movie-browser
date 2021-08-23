0a5c20aa9d9966e67e7a0c70c47abea8b3987489
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-23 15:14:40 +0200

    simplify function

--- src/features/movies/MoviesPage/useGetMovieGenres.js
+++ src/features/movies/MoviesPage/useGetMovieGenres.js
@@ -7,14 +7,10 @@ export const useGetMovieGenres = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getMovieGenres = () => {
+        fetch(apiURL)
+            .then(response => response.json())
+            .then(genres => dispatch(setGenres(genres)))
 
-            fetch(apiURL)
-                .then(response => response.json())
-                .then(genres => dispatch(setGenres(genres)))
-        };
-
-        getMovieGenres();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 };
