7ad70c99fa48559be1c6cac61d5802f36b3e807b
Kamil Stawik <kamil_stawik@op.pl>
2021-08-23 22:42:58 +0200

    LoadingPage moved to common

--- src/features/LoadingPage/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Header = styled.header`
-    font-weight: 600;
-    font-size: 36px;
-    line-height: 1.2;
-    color: ${({ theme }) => theme.color.woodsmoke};
-`;
