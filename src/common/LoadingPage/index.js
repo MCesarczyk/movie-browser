7c8fe40ccd92b48c2438a32b23d403fd473d0c28
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-23 23:11:54 +0200

    blank line removing

--- src/common/LoadingPage/index.js
+++ src/common/LoadingPage/index.js
@@ -3,7 +3,6 @@ import LoadingCircle from "./LoadingCircle";
 import { Header } from "./styled";
 
 const LoadingPage = () => {
-
     return (
         <Wrapper>
             <Header>Search results for “Mulan”</Header>
