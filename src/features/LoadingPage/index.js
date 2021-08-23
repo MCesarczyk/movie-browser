c45b86d3d9769875083eca08253d59a998d61c99
Kamil Stawik <kamil_stawik@op.pl>
2021-08-23 19:09:42 +0200

    Header added to LoadingPage

---
+++ src/features/LoadingPage/index.js
@@ -0,0 +1,14 @@
+import Wrapper from "../../common/Wrapper";
+import { Header } from "./styled";
+
+const LoadingPage = () => {
+
+    return (
+        <Wrapper>
+            <Header>Search results for “Mulan”</Header>
+
+        </Wrapper>
+    );
+};
+
+export default LoadingPage;
