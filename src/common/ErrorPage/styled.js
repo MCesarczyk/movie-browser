5064542fd373579334646653b7f93ce070a27a9f
Justyna Pawlik <juspaw11@wp.pl>
2021-08-24 10:25:45 +0200

    added font-size in error prompt

--- src/common/ErrorPage/styled.js
+++ src/common/ErrorPage/styled.js
@@ -9,8 +9,17 @@ padding: 0px;
 
 export const ErrorIcon = styled.img``;
 
-export const Header = styled.header``;
+export const Header = styled.header`
+font-size: 36px;
 
-export const Section = styled.p``;
+`;
 
-export const ButtonLink = styled.button``;
+export const Section = styled.p`
+font-size: 22px;
+
+  `;
+
+export const ButtonLink = styled.button`
+font-size: 14px;
+font-weight: bold;
+`;
