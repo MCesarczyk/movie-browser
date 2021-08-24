7a8315b4b388e6770ad215db0962c309afdff165
Justyna Pawlik <juspaw11@wp.pl>
2021-08-24 10:13:20 +0200

    put ErrorPage in the  in the middle of the website

--- src/common/ErrorPage/styled.js
+++ src/common/ErrorPage/styled.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
-export const ErrorPageWrapper = styled.div``;
+export const ErrorPageWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 0px;
+`;
 
 export const ErrorIcon = styled.img``;
 
