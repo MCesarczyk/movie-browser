ec4a787f6e9d647a506a7afb43bbddf2139a9a38
Kamil Stawik <kamil_stawik@op.pl>
2021-08-24 18:03:04 +0200

    basic logic added to Pager

--- src/common/Pager/index.js
+++ src/common/Pager/index.js
@@ -1,16 +1,21 @@
 import { Wrapper, Button, PagerText, PageNumberText } from "./styled";
 
-const Pager = () => (
-    <Wrapper>
-        <Button>First</Button>
-        <Button>Previous</Button>
-        <PagerText>Page</PagerText>
-        <PageNumberText>1</PageNumberText>
-        <PagerText>of</PagerText>
-        <PageNumberText>500</PageNumberText>
-        <Button>Next</Button>
-        <Button>Last</Button>
-    </Wrapper>
-);
+const Pager = () => {
+    const pageNumber = 1;
+    const lastPageNumber = 500;
+
+    return (
+        <Wrapper>
+            <Button disabled={pageNumber === 1 ? true : false}>First</Button>
+            <Button disabled={pageNumber === 1 ? true : false}>Previous</Button>
+            <PagerText>Page</PagerText>
+            <PageNumberText>{pageNumber}</PageNumberText>
+            <PagerText>of</PagerText>
+            <PageNumberText>{lastPageNumber}</PageNumberText>
+            <Button disabled={pageNumber === lastPageNumber ? true : false}>Next</Button>
+            <Button disabled={pageNumber === lastPageNumber ? true : false}>Last</Button>
+        </Wrapper>
+    );
+};
 
 export default Pager;
