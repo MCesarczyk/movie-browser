14ff243c5a8318a71304257c1fc9c3245836859e
Kamil Stawik <kamil_stawik@op.pl>
2021-08-24 16:19:01 +0200

    Pager installed in MoviesPage

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -6,6 +6,7 @@ import { MoviesList } from "./styled";
 import LoadingPage from "../../../common/LoadingPage";
 import ErrorPage from "../../../common/ErrorPage";
 import { useGetMovieGenres } from "./useGetMovieGenres";
+import Pager from "../../../common/Pager";
 
 const MoviesPage = () => {
     useGetPopularMovies();
@@ -15,27 +16,30 @@ const MoviesPage = () => {
     const genres = useSelector(selectGenres);
 
     return (
-        <MoviesList title="Movies">
-            {moviesState === "loading" ? (
-                <LoadingPage />
-            ) : (
-                moviesState === "Error" ? (
-                    <ErrorPage />
+        <>
+            <MoviesList title="Movies">
+                {moviesState === "loading" ? (
+                    <LoadingPage />
                 ) : (
-                    movies.movies.map((movie, index) => (
-                        <Tile
-                            key={movies.movies[index].id}
-                            title={movies.movies[index].title}
-                            subtitle={new Date(Date.parse(movies.movies[index].release_date)).getFullYear()}
-                            releaseDate={movies.movies[index].release_date}
-                            genreIds={movies.movies[index].genre_ids}
-                            rating={movies.movies[index].vote_average}
-                            votes={movies.movies[index].vote_count}
-                            overview={movies.movies[index].overview}
-                            posterUrl={`https://image.tmdb.org/t/p/w500${movies.movies[index].poster_path}`}
-                        />
-                    ))))}
-        </MoviesList>
+                    moviesState === "Error" ? (
+                        <ErrorPage />
+                    ) : (
+                        movies.movies.map((movie, index) => (
+                            <Tile
+                                key={movies.movies[index].id}
+                                title={movies.movies[index].title}
+                                subtitle={new Date(Date.parse(movies.movies[index].release_date)).getFullYear()}
+                                releaseDate={movies.movies[index].release_date}
+                                genreIds={movies.movies[index].genre_ids}
+                                rating={movies.movies[index].vote_average}
+                                votes={movies.movies[index].vote_count}
+                                overview={movies.movies[index].overview}
+                                posterUrl={`https://image.tmdb.org/t/p/w500${movies.movies[index].poster_path}`}
+                            />
+                        ))))}
+            </MoviesList>
+            <Pager />
+        </>
     )
 };
 
