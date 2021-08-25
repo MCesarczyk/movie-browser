4c1dca0faac001850213d3a009ab160ce128c9ba
Justyna Pawlik <juspaw11@wp.pl>
2021-08-25 08:05:46 +0200

    added useHistory to Search

--- src/features/Navigation/Search/index.js
+++ src/features/Navigation/Search/index.js
@@ -4,11 +4,27 @@ import { useLocation } from "react-router-dom";
 
 export const Search = () => {
     const location = useLocation();
+    const history = useHistory();
     const query = (new URLSearchParams(location.search)).get("szukaj");
+
+    const onInputChange = ({ target }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if(target.value.trim() === "") {
+            searchParams.delete("szukaj");
+        } else {
+            searchParams.set("szukaj", target.value);
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+        };
     return (
         <SearchWrapper>
             <SearchIcon src={search} alt="" />
-            <SearchInput placeholder="Search for movies..." />
+            <SearchInput placeholder="Search for movies..." 
+            value={query || ""}
+            onChange={onInputChange}
+            />
         </SearchWrapper>    
     )
 }
