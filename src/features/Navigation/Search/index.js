e47855933bd902e936271d1d6f5473c4eb8a23ae
Justyna Pawlik <juspaw11@wp.pl>
2021-08-25 08:18:23 +0200

    added searchQueryParamName

--- src/features/Navigation/Search/index.js
+++ src/features/Navigation/Search/index.js
@@ -1,19 +1,20 @@
 import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 import search from "../images/search.svg";
 import { useHistory, useLocation } from "react-router-dom";
+import searchQueryParamName from "./searchQueryParamName";
 
 export const Search = () => {
     const location = useLocation();
     const history = useHistory();
-    const query = (new URLSearchParams(location.search)).get("szukaj");
+    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
 
     const onInputChange = ({ target }) => {
         const searchParams = new URLSearchParams(location.search);
 
         if(target.value.trim() === "") {
-            searchParams.delete("szukaj");
+            searchParams.delete(searchQueryParamName);
         } else {
-            searchParams.set("szukaj", target.value);
+            searchParams.set(searchQueryParamName, target.value);
         }
 
         history.push(`${location.pathname}?${searchParams.toString()}`);
