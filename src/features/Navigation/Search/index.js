41646fa72026b5e8b3dabcc8d2917a9d59e94cae
Justyna Pawlik <juspaw11@wp.pl>
2021-08-25 07:56:16 +0200

    added useLocation in Search

--- src/features/Navigation/Search/index.js
+++ src/features/Navigation/Search/index.js
@@ -1,7 +1,10 @@
 import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 import search from "../images/search.svg";
+import { useLocation } from "react-router-dom";
 
 export const Search = () => {
+    const location = useLocation();
+    const query = (new URLSearchParams(location.search)).get("szukaj");
     return (
         <SearchWrapper>
             <SearchIcon src={search} alt="" />
