ce307fa19fb58389932a6c9230db10203d8fb421
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-08-26 09:31:55 +0200

    add styles to page

--- src/features/movies/MoviePage/styled.js
+++ src/features/movies/MoviePage/styled.js
@@ -1,11 +1,5 @@
 import styled from "styled-components";
 
-export const MoviesList = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 24px;
-`;
-
 export const SectionTitle = styled.h2`
     font-weight: 600;
     font-size: 36px;
