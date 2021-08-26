ee28accec5d1d7740672585854f5424d784590bb
Kamil Stawik <kamil_stawik@op.pl>
2021-08-26 13:39:27 +0200

    two chacking functions added to Pager

--- src/common/Pager/index.js
+++ src/common/Pager/index.js
@@ -5,19 +5,22 @@ const Pager = () => {
     const pageNumber = 1;
     const lastPageNumber = 500;
 
+    const checkIfPreviousIsDisabled = () => pageNumber === 1 ? true : false;
+    const checkIfNextIsDisabled = () => pageNumber === lastPageNumber ? true : false;
+
     return (
         <Wrapper>
-            <Button disabled={pageNumber === 1 ? true : false}>First</Button>
-            <Button disabled={pageNumber === 1 ? true : false}>Previous</Button>
+            <Button disabled={checkIfPreviousIsDisabled()}>First</Button>
+            <Button disabled={checkIfPreviousIsDisabled()}>Previous</Button>
             <PagerText>Page</PagerText>
             <PageNumberText>{pageNumber}</PageNumberText>
             <PagerText>of</PagerText>
             <PageNumberText>{lastPageNumber}</PageNumberText>
-            <Button disabled={pageNumber === lastPageNumber ? true : false}>
+            <Button disabled={checkIfNextIsDisabled()}>
                 Next
                 <NextIcon />
             </Button>
-            <Button disabled={pageNumber === lastPageNumber ? true : false}>
+            <Button disabled={checkIfNextIsDisabled()}>
                 Last
                 <NextIcon />
             </Button>
