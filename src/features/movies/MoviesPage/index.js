583c1b51421c65b454367fd7daa8c8aeb0af6863
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-27 02:07:57 +0200

    Update index.js

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -10,30 +10,7 @@ import { selectMovies, selectMoviesState } from "../moviesSlice";
 import { selectImagesBaseURL, selectPosterSizes, selectPosterSize, setPosterSize } from "../../../configSlice";
 
 const MoviesPage = () => {
-    useGetConfig();
-    useGetMovieGenres();
-    useGetPopularMovies();
-    const imgURL = useSelector(selectImagesBaseURL);
-    const posterSizes = useSelector(selectPosterSizes);
-    const moviesState = useSelector(selectMoviesState);
-    const movies = useSelector(selectMovies);
-    const posterSize = useSelector(selectPosterSize);
-    const dispatch = useDispatch();
-
-    const onPageResize = () => {
-        const maxwidth = window.innerWidth;
-        if (maxwidth > "1280") {
-            dispatch(setPosterSize(posterSizes[4]))
-        } else if (maxwidth > "768") {
-            dispatch(setPosterSize(posterSizes[3]))
-        } else if (maxwidth > "480") {
-            dispatch(setPosterSize(posterSizes[2]))
-        } else {
-            dispatch(setPosterSize(posterSizes[1]))
-        };
-    };
-
-    window.addEventListener("resize", onPageResize);
+    
 
     return (
         <MoviesList title="Movies" >
@@ -45,15 +22,15 @@ const MoviesPage = () => {
                 ) : (
                     movies.movies.map((movie, index) => (
                         <Tile
-                            key={movies.movies[index].id}
-                            title={movies.movies[index].title}
-                            subtitle={new Date(Date.parse(movies.movies[index].release_date)).getFullYear()}
-                            releaseDate={movies.movies[index].release_date}
-                            genreIds={movies.movies[index].genre_ids}
-                            rating={movies.movies[index].vote_average}
-                            votes={movies.movies[index].vote_count}
-                            overview={movies.movies[index].overview}
-                            posterUrl={`${imgURL}${posterSize}${movies.movies[index].poster_path}`}
+                            // key={movies.movies[index].id}
+                            // title={movies.movies[index].title}
+                            // subtitle={new Date(Date.parse(movies.movies[index].release_date)).getFullYear()}
+                            // releaseDate={movies.movies[index].release_date}
+                            // genreIds={movies.movies[index].genre_ids}
+                            // rating={movies.movies[index].vote_average}
+                            // votes={movies.movies[index].vote_count}
+                            // overview={movies.movies[index].overview}
+                            // posterUrl={`${imgURL}${posterSize}${movies.movies[index].poster_path}`}
                         />
                     ))))}
         </MoviesList>
