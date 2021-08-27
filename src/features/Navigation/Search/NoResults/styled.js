84525687c339b2a436778ac841624f67f9a09777
Justyna Pawlik <juspaw11@wp.pl>
2021-08-27 14:06:20 +0200

    changed media queries for header

--- src/features/Navigation/Search/NoResults/styled.js
+++ src/features/Navigation/Search/NoResults/styled.js
@@ -6,12 +6,12 @@ font-size: 36px;
 line-height: 1.2;
 margin-top: 50px;
 @media(max-width: ${({theme}) => theme.breakpoint.mobileMin}px) {
-    font-size: 26px;
-    margin-top: 30px;
-  }
-  @media(max-width: ${({theme}) => theme.breakpoint.mobile}px) {
     font-size: 20px;
     margin-top: 20px;
+  }
+  @media(max-width: ${({theme}) => theme.breakpoint.mobile}px) {
+    font-size: 26px;
+    margin-top: 30px;
   `;
 
 export const NoResultsLogo = styled.img`
