cc07a572f37279193ffc5b705f82c95dbb2f6891
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-08-27 13:23:49 +0200

    Create moviesSaga.js

---
+++ src/features/movies/moviesSaga.js
@@ -0,0 +1,19 @@
+import { call, delay, put, takeLatest } from "redux-saga/effects";
+import { getPopularMovies } from "./getPopularMovies";
+import { fetchPopularMovies, setMovies, setState } from "./moviesSlice";
+
+function* fetchPopularMoviesHandler() {
+    try {
+        const movies = yield call(getPopularMovies);
+        yield put(setMovies(movies));
+        yield put(setState("loading"));
+        yield delay(2_000);
+        yield put(setState("success"));
+    } catch (error) {
+        yield call(alert, "Unable to fetch data...");
+    }
+}
+
+export function* moviesSaga() {
+    yield takeLatest(fetchPopularMovies.type, fetchPopularMoviesHandler);
+}
