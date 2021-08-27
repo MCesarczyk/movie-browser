07a6fb579cce73157920f5ee2035ca231fc826b6
Justyna Pawlik <juspaw11@wp.pl>
2021-08-27 11:52:26 +0200

    imported picture in noResults site

--- src/features/Navigation/Search/NoResults/index.js
+++ src/features/Navigation/Search/NoResults/index.js
@@ -1,10 +1,13 @@
-import { Content } from "./styled";
-
+import { Content, NoResultsLogo } from "./styled";
+import noResults from "./noResults.svg";
 
 export const NoResults = () => {
     return (
-<Content>
-    Sorry, there are no results for "Muan"
-</Content>
+        <>
+    <Content>
+        Sorry, there are no results for "Muan"
+    </Content>
+    <NoResultsLogo src={noResults} alt="" />
+</>
     );
 };
--- src/features/Navigation/Search/NoResults/styled.js
+++ src/features/Navigation/Search/NoResults/styled.js
@@ -5,3 +5,5 @@ font-weight: 600;
 font-size: 36px;
 line-height: 1.2;
 `;
+
+export const NoResultsLogo = styled.img``;
