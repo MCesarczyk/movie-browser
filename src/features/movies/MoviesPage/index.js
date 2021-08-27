8129ce06b81b9232e685a2109ec300716517b237
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-08-27 08:56:43 +0200

    triggering of initial parameters setting

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { applicationStart } from "../moviesSlice";
+
 const MoviesPage = () => {
-    
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(applicationStart());
+    }, []);
 
     return (
         <div>test</div>
