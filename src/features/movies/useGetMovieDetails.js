1dec31f1df6fdaa8d5775d4ece6ae84d9e24b1e3
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-08-30 10:49:25 +0200

    added language parameter to apiURL in hooks

--- src/features/movies/useGetMovieDetails.js
+++ src/features/movies/useGetMovieDetails.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { setMovie } from "./moviesSlice";
 
 export const useGetMovieDetails = (movie_id) => {
-    const apiURL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=768f7875782193f5e4797762314da0b7`;
+    const apiURL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=768f7875782193f5e4797762314da0b7&language=en-US`;
     const dispatch = useDispatch();
 
     useEffect(() => {
