0601aa678cf367c81ad7073186aa94e490c39e2b
Kamil Stawik <kamil_stawik@op.pl>
2021-08-30 22:52:43 +0200

    useGetPopularMovies rebuild + setTimeout added

--- src/features/movies/useGetPopularMovies.js
+++ src/features/movies/useGetPopularMovies.js
@@ -1,19 +1,35 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { selectPage, setMovies, setTotalPages } from "./moviesSlice";
+import { useParams } from "react-router-dom";
+import { setMovies, setTotalPages, setState } from "./moviesSlice";
 
 export const useGetPopularMovies = () => {
-    const page = useSelector(selectPage);
-    const apiURL = `https://api.themoviedb.org/3/movie/popular?api_key=768f7875782193f5e4797762314da0b7&page=${page}`;
+
     const dispatch = useDispatch();
+    const params = useParams();
+    const page = (params.page ? params.page : 1);
+    const apiURL = `https://api.themoviedb.org/3/movie/popular?api_key=768f7875782193f5e4797762314da0b7&page=${page}`;
 
     useEffect(() => {
-        fetch(apiURL)
-            .then(response => response.json())
-            .then(movies => {
-                dispatch(setMovies(movies.results))
-                dispatch(setTotalPages(movies.total_pages))
-            });
-    }, []);
+        dispatch(setState("loading"));
+        setTimeout(() => {
+            fetch(apiURL)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText);
+                    }
+                    return response;
+                })
+                .then(response => response.json())
+                .then(movies => {
+                    dispatch(setMovies(movies.results))
+                    dispatch(setTotalPages(movies.total_pages))
+                })
+                .then(dispatch(setState("success")))
+                .catch(error => {
+                    dispatch(setState("Error"))
+                    console.error("Niestety coś się zepsuło", error)
+                });
+        }, 1500);
+    }, [page]);
 };
