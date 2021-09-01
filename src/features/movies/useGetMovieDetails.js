74620542bb7174314f8b9967f105c5abc91b4b7c
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-01 06:55:59 +0200

    take movieId value from router

--- src/features/movies/useGetMovieDetails.js
+++ src/features/movies/useGetMovieDetails.js
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 import { setMovie } from "./moviesSlice";
 
-export const useGetMovieDetails = (movie_id) => {
-    const apiURL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=768f7875782193f5e4797762314da0b7`;
+export const useGetMovieDetails = () => {
+    const { id } = useParams();
+    const movieId = id;
+    const apiURL = `https://api.themoviedb.org/3/movie/${movieId}?api_key=768f7875782193f5e4797762314da0b7`;
     const dispatch = useDispatch();
 
     useEffect(() => {
