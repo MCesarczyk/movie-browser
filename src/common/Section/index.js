850bf7d235be04fc5661b7082390110f323e511f
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-03 12:20:48 +0200

    name of container changed

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,12 +1,12 @@
-import { SectionContainer, SectionTitle, SectionWrapper } from "./styled";
+import { SectionListContainer, SectionTitle, SectionWrapper } from "./styled";
 
 const Section = ({ title, body }) => {
     return (
         <SectionWrapper>
             <SectionTitle>{title}</SectionTitle>
-            <SectionContainer>
+            <SectionListContainer>
                 {body}
-            </SectionContainer>
+            </SectionListContainer>
         </SectionWrapper>
     )
 };
