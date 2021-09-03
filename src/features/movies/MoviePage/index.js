dcceb8a1f1ba07db0a64da3f7e9d3ed1227b9d2d
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-03 08:03:41 +0200

    profile size connected to MoviePage

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -16,8 +16,13 @@ import {
     selectPosterSize,
     selectPosterSizes,
     selectPosterWidth,
+    selectProfileSize,
+    selectProfileSizes,
+    selectProfileWidth,
     setPosterSize,
-    setPosterWidth
+    setPosterWidth,
+    setProfileSize,
+    setProfileWidth
 } from "../../../globalSlice";
 import { useGetConfig } from "../../../useGetConfig";
 import { useGetMovieDetails } from "../useGetMovieDetails";
@@ -31,9 +36,12 @@ const MoviePage = () => {
     const movieId = id;
     const movieDetails = useSelector(selectMovieDetails);
     const imgURL = useSelector(selectImagesBaseURL);
+    const posterSizes = useSelector(selectPosterSizes);
     const posterSize = useSelector(selectPosterSize);
     const posterWidth = useSelector(selectPosterWidth);
-    const posterSizes = useSelector(selectPosterSizes);
+    const profileSizes = useSelector(selectProfileSizes);
+    const profileSize = useSelector(selectProfileSize);
+    const profileWidth = useSelector(selectProfileWidth);
     const movieCast = useSelector(selectMovieCast);
     const movieCrew = useSelector(selectMovieCrew);
     const genresList = useSelector(selectGenresList);
@@ -51,15 +59,23 @@ const MoviePage = () => {
         if (maxwidth > "1280") {
             dispatch(setPosterSize(posterSizes[4]));
             dispatch(setPosterWidth("312"));
+            dispatch(setProfileSize(profileSizes[2]));
+            dispatch(setProfileWidth("177"));
         } else if (maxwidth > "768") {
             dispatch(setPosterSize(posterSizes[3]))
             dispatch(setPosterWidth("240"));
+            dispatch(setProfileSize(profileSizes[1]));
+            dispatch(setProfileWidth("120"));
         } else if (maxwidth > "480") {
             dispatch(setPosterSize(posterSizes[2]))
             dispatch(setPosterWidth("180"));
+            dispatch(setProfileSize(profileSizes[1]));
+            dispatch(setProfileWidth("120"));
         } else {
             dispatch(setPosterSize(posterSizes[1]))
             dispatch(setPosterWidth("114"));
+            dispatch(setProfileSize(profileSizes[1]));
+            dispatch(setProfileWidth("120"));
         };
     };
 
@@ -96,10 +112,10 @@ const MoviePage = () => {
                         body={movieCast && movieCast.map((person, index) => (
                             <Tile
                                 minimal
-                                width={posterWidth}
+                                width={profileWidth}
                                 key={movieCast[index].credit_id}
                                 titleUrl={`/people/${movieCast[index].id}`}
-                                imageUrl={`${imgURL}${posterSize}${movieCast[index].profile_path}`}
+                                imageUrl={`${imgURL}${profileSize}${movieCast[index].profile_path}`}
                                 title={movieCast[index].name}
                                 subtitle={movieCast[index].character}
                             />
@@ -110,10 +126,10 @@ const MoviePage = () => {
                         body={movieCrew && movieCrew.map((person, index) => (
                             <Tile
                                 minimal
-                                width={posterWidth}
+                                width={profileWidth}
                                 key={movieCrew[index].credit_id}
                                 titleUrl={`/people/${movieCrew[index].id}`}
-                                imageUrl={`${imgURL}${posterSize}${movieCrew[index].profile_path}`}
+                                imageUrl={`${imgURL}${profileSize}${movieCrew[index].profile_path}`}
                                 title={movieCrew[index].name}
                                 subtitle={movieCrew[index].job}
                             />
