bcaa34c83195210557005eb142dbb5ad2a5f00c2
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-03 07:55:46 +0200

    add profile size property to slice

--- src/globalSlice.js
+++ src/globalSlice.js
@@ -17,6 +17,8 @@ const initialState = {
     state: "idle",
     posterSize: "w500",
     posterWidth: "312",
+    profileSize: "h632",
+    profileWidth: "177",
     backdropSize: "w1280",
 };
 
@@ -32,6 +34,12 @@ const globalSlice = createSlice({
         },
         setPosterWidth: (state, { payload: newWidth }) => {
             state.posterWidth = newWidth;
+        },
+        setProfileSize: (state, { payload: newSize }) => {
+            state.profileSize = newSize;
+        },
+        setProfileWidth: (state, { payload: newWidth }) => {
+            state.profileWidth = newWidth;
         },
         setBackdropSize: (state, { payload: newSize }) => {
             state.backdropSize = newSize;
@@ -49,6 +57,8 @@ export const {
     setConfig,
     setPosterSize,
     setPosterWidth,
+    setProfileSize,
+    setProfileWidth,
     setBackdropSize,
     setTotalPages,
     setState,
@@ -58,9 +68,12 @@ export const selectConfig = state => state.global;
 export const selectImagesConfig = state => state.global.config.images;
 export const selectImagesBaseURL = state => state.global.config.images.secure_base_url;
 export const selectPosterSizes = state => state.global.config.images.poster_sizes;
+export const selectProfileSizes = state => state.global.config.images.profile_sizes;
 export const selectBackdropSizes = state => state.global.config.images.backdrop_sizes;
 export const selectPosterSize = state => state.global.posterSize;
 export const selectPosterWidth = state => state.global.posterWidth;
+export const selectProfileSize = state => state.global.profileSize;
+export const selectProfileWidth = state => state.global.profileWidth;
 export const selectBackdropSize = state => state.global.backdropSize;
 export const selectTotalPages = state => state.global.totalPages;
 export const selectState = state => state.global.state;
