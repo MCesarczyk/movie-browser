dd23d78b7b167eb09cc5085b8d70fe21063df0d6
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-03 13:41:12 +0200

    pre-merging errors fixed, conflicts resolved

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -57,25 +57,23 @@ const MoviesPage = () => {
                 <TileList>
                     {moviesState === "loading" && <LoadingPage message="Loading movies list..." />}
                     {moviesState === "error" && <ErrorPage />}
-                        <MoviesList title="Movies" >
-                            {moviesState === "success" && movieList && movieList.map((movie, index) => (
-                                <Tile
-                                    imageWidth="100%"
-                                    mobile="114"
-                                    movieId={movieList[index].id}
-                                    key={movieList[index].id}
-                                    titleUrl={`/movie/${movieList[index].id}`}
-                                    imageUrl={`${imgURL}${posterSize}${movieList[index].poster_path}`}
-                                    title={movieList[index].title}
-                                    subtitle={new Date(Date.parse(movieList[index].release_date)).getFullYear()}
-                                    genreIds={movieList[index].genre_ids}
-                                    rating={movieList[index].vote_average}
-                                    votes={movieList[index].vote_count}
-                                />
-                            ))}
-                        </MoviesList>
-                </TileList>
-                    {moviesState === "success" && <Pager />}
+                        {moviesState === "success" && movieList && movieList.map((movie, index) => (
+                            <Tile
+                                imageWidth="100%"
+                                mobile="114"
+                                movieId={movieList[index].id}
+                                key={movieList[index].id}
+                                titleUrl={`/movie/${movieList[index].id}`}
+                                imageUrl={`${imgURL}${posterSize}${movieList[index].poster_path}`}
+                                title={movieList[index].title}
+                                subtitle={new Date(Date.parse(movieList[index].release_date)).getFullYear()}
+                                genreIds={movieList[index].genre_ids}
+                                rating={movieList[index].vote_average}
+                                votes={movieList[index].vote_count}
+                            />
+                        ))}
+                    </TileList>
+                {moviesState === "success" && <Pager />}
             </Wrapper>
         </>
     )
