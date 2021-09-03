9e1d738c8c83b7d50486f20f1f808c14d5612afe
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-03 20:57:03 +0200

    the same adjustments with Slides section

--- src/common/SlidesSection/styled.js
+++ src/common/SlidesSection/styled.js
@@ -2,14 +2,29 @@ import styled from "styled-components";
 
 export const SectionWrapper = styled.div`
     width: 100%;
-    margin: 64px auto;
+    margin: 0 auto;
 `;
 
 export const SectionTitle = styled.h2`
     font-weight: 600;
     font-size: 36px;
     line-height: 1.2;
-    margin-top: 8px;
+    margin-top: 64px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mediumScreen}) {
+        font-size: 30px;
+        margin-top: 50px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+        font: 24px;
+        margin-top: 36px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.oldIphone}) {
+        font-size: 18px;
+        margin-top: 24px;
+    }
 `;
 
 export const SectionMiddleContainer = styled.div`
