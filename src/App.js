8d31808c26c12261bae3d6f71c8e57d831b15147
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-06 22:28:47 +0200

    configuration saga started with App loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Container } from './common/Container';
 import Navigation from './features/Navigation';
 import MoviesPage from './features/movies/MoviesPage';
+import { useDispatch } from 'react-redux';
+import { fetchConfiguration } from './globalSlice';
 // import PeopleList from './features/people/PeoplePage';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchConfiguration());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <HashRouter>
       <Container>
