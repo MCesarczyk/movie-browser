16413b182b49cd6b3910ea861bc006289ed90d56
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-06 15:54:00 +0200

    Revert "Merge branch 'main' into component/TileList-Tile-responsiveness"
    This reverts commit e06a4f28b89a894aa6f27091d1c4f6c6ded3e045, reversing
    changes made to 1e1e518675195f87cc098aca85b96b00236bfd9f.

--- src/globalSlice.js
+++ src/globalSlice.js
@@ -15,8 +15,6 @@ const initialState = {
     },
     totalPages: 500,
     state: "idle",
-    posterSize: "w500",
-    backdropSize: "w1280",
 };
 
 const globalSlice = createSlice({
@@ -25,12 +23,6 @@ const globalSlice = createSlice({
     reducers: {
         setConfig: (state, { payload: newConfig }) => {
             state.config = newConfig;
-        },
-        setPosterSize: (state, { payload: newSize }) => {
-            state.posterSize = newSize;
-        },
-        setBackdropSize: (state, { payload: newSize }) => {
-            state.backdropSize = newSize;
         },
         setTotalPages: (state, { payload: totalPages }) => {
             state.totalPages = totalPages;
@@ -43,8 +35,6 @@ const globalSlice = createSlice({
 
 export const {
     setConfig,
-    setPosterSize,
-    setBackdropSize,
     setTotalPages,
     setState,
 } = globalSlice.actions;
@@ -53,9 +43,8 @@ export const selectConfig = state => state.global;
 export const selectImagesConfig = state => state.global.config.images;
 export const selectImagesBaseURL = state => state.global.config.images.secure_base_url;
 export const selectPosterSizes = state => state.global.config.images.poster_sizes;
+export const selectProfileSizes = state => state.global.config.images.profile_sizes;
 export const selectBackdropSizes = state => state.global.config.images.backdrop_sizes;
-export const selectPosterSize = state => state.global.posterSize;
-export const selectBackdropSize = state => state.global.backdropSize;
 export const selectTotalPages = state => state.global.totalPages;
 export const selectState = state => state.global.state;
 
