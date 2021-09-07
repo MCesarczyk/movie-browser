06199892122c646aac71bdc20ca017169fc9e3d5
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-07 12:58:06 +0200

    added passing page number from store to API request

--- src/features/movies/getPopularMovies.js
+++ src/features/movies/getPopularMovies.js
@@ -1,5 +1,7 @@
-export const getPopularMovies = async () => {
-    const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=768f7875782193f5e4797762314da0b7");
+export const getPopularMovies = async (page) => {
+    const apiURL = `https://api.themoviedb.org/3/movie/popular?api_key=768f7875782193f5e4797762314da0b7&page=${page}&language=en-US`;
+
+    const response = await fetch(apiURL);
 
     if (!response.ok) {
         new Error(response.statusText);
--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -1,14 +1,16 @@
-import { call, delay, put, takeLatest } from "redux-saga/effects";
-import { setError, setState } from "../../globalSlice";
+import { call, delay, put, select, takeLatest } from "redux-saga/effects";
+import { selectPage, setError, setState, setTotalPages } from "../../globalSlice";
 import { getPopularMovies } from "./getPopularMovies";
 import { fetchPopularMovies, setMovies } from "./moviesSlice";
 
 function* fetchPopularMoviesHandler() {
     try {
-        const movies = yield call(getPopularMovies);
-        yield put(setMovies(movies));
         yield put(setState("loading"));
-        yield delay(2_000);
+        const page = yield select(selectPage);
+        const movies = yield call(getPopularMovies, page);
+        yield put(setMovies(movies.results));
+        yield put(setTotalPages(movies.total_pages));
+        yield delay(1_000);
         yield put(setState("success"));
     } catch (error) {
         yield call(setError(error));
