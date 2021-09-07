afc8593e1e3cac3145fd392e8a57901e9f492780
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-07 20:26:42 +0200

    format globalSaga

--- src/globalSaga.js
+++ src/globalSaga.js
@@ -3,9 +3,8 @@ import { getDataFromApi } from "./getDataFromApi";
 import { fetchConfiguration, setConfig, setError } from "./globalSlice";
 
 function* fetchConfigurationHandler() {
-    const apiURL = `https://api.themoviedb.org/3/configuration?api_key=768f7875782193f5e4797762314da0b7&language=en-US`;
-    
     try {
+        const apiURL = `https://api.themoviedb.org/3/configuration?api_key=768f7875782193f5e4797762314da0b7&language=en-US`;
         const config = yield call(getDataFromApi, apiURL);
         yield put(setConfig(config));
     } catch (error) {
