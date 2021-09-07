f857773e79d09154a9345e1e6ffcad64085c9a31
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-07 23:51:46 +0200

    connect peopleSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import { moviesSaga } from "./features/movies/moviesSaga";
+import { peopleSaga } from "./features/people/peopleSaga";
 import { globalSaga } from "./globalSaga";
 
 export default function* rootSaga() {
     yield all([
         moviesSaga(),
+        peopleSaga(),
         globalSaga(),
     ]);
 }
