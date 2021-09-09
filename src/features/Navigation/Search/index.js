dfbfdc22964d112461c1f938cf94828ba5c6339a
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-09 07:02:23 +0200

    pass query parameters through store

--- src/features/Navigation/Search/index.js
+++ src/features/Navigation/Search/index.js
@@ -2,22 +2,23 @@ import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 import search from "../images/search.svg";
 import { useHistory, useLocation } from "react-router-dom";
 import searchQueryParamName from "./searchQueryParamName";
+import { useDispatch, useSelector } from "react-redux";
+import { selectQuery, setQuery } from "../../../globalSlice";
+import { useEffect } from "react";
 
 export const Search = () => {
-    const location = useLocation();
+    const dispatch = useDispatch();
     const history = useHistory();
-    const searchParams = new URLSearchParams(location.search);
-    const query = searchParams.get(searchQueryParamName);
+    const query = useSelector(selectQuery);
 
     const onInputChange = ({ target }) => {
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value);
-        }
-
-        history.push(`/movies/1?${searchParams.toString()}`);
+            dispatch(setQuery(target.value));
     };
+
+    useEffect(() => {
+        query && query !== null && history.push(`/movies/1?${searchQueryParamName}=${query}`);
+    }, [query]);
+
     return (
         <SearchWrapper>
             <SearchIcon src={search} alt="" />
