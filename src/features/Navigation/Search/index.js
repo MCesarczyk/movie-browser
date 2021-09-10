6a3c3a48f3eb9b3e21339feac396c1f00e1cacbe
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-10 06:34:24 +0200

    Restore search params control by router, add property to make Search component generic
    Revert changes made in commit dfbfdc22964d112461c1f938cf94828ba5c6339a

--- src/features/Navigation/Search/index.js
+++ src/features/Navigation/Search/index.js
@@ -1,30 +1,28 @@
-import { useEffect } from "react";
-import { useRouteMatch } from "react-router";
+import { useLocation, useRouteMatch } from "react-router";
 import { useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
 import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 import search from "../images/search.svg";
 import searchQueryParamName from "./searchQueryParamName";
-import { selectQuery, setQuery } from "../../../globalSlice";
-import { useState } from "react";
 
 export const Search = () => {
-    const dispatch = useDispatch();
+    const location = useLocation();
     const history = useHistory();
-    const query = useSelector(selectQuery);
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get(searchQueryParamName);
 
     const moviesMatch = useRouteMatch("/movies");
     const peopleMatch = useRouteMatch("/people");
-    const property = moviesMatch && moviesMatch.path || peopleMatch && peopleMatch.path;
-    
+    const property = (moviesMatch && moviesMatch.path) || (peopleMatch && peopleMatch.path);
+
     const onInputChange = ({ target }) => {
-        dispatch(setQuery(target.value));
+        if (target.value.trim() === "") {
+            searchParams.delete(searchQueryParamName);
+        } else {
+            searchParams.set(searchQueryParamName, target.value);
+        }
+
+        history.push(`${property}/1?${searchParams.toString()}`);
     };
-
-    useEffect(() => {
-        query && query !== null && history.push(`${property}/1?${searchQueryParamName}=${query}`);
-    }, [query]);
-
     return (
         <SearchWrapper>
             <SearchIcon src={search} alt="" />
