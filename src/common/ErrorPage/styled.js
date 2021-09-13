d6845ec10cf64b0bb43663af6b7efbd54654d487
Michał Cesarczyk CNC <83251458+MCesarczyk@users.noreply.github.com>
2021-09-13 12:25:20 +0200

    unify styles for Loading, Error and NoResults pages

--- src/common/ErrorPage/styled.js
+++ src/common/ErrorPage/styled.js
@@ -2,63 +2,82 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const ErrorPageWrapper = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-text-align: center;
-position: absolute;
-top: 50%;
-left: 50%;
-margin-right: -50%;
-transform: translate(-50%, -50%)
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
 `;
 
 export const ErrorIcon = styled.img``;
 
-export const Header = styled.header`
-  font-size: 36px;
-  line-height: 120%;
+export const Header = styled.h2`
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 1.2;
+    margin-top: 64px;
 
-  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMin}) {
-    font-size: 28px;
-  }
+    @media (max-width: ${({ theme }) => theme.breakpoint.mediumScreen}) {
+        font-size: 30px;
+        margin-top: 50px;
+    }
 
-  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
-    font-size: 18px;
-  }
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+        font: 24px;
+        margin-top: 36px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.oldIphone}) {
+        font-size: 18px;
+        margin-top: 24px;
+    }
 `;
 
 export const Section = styled.p`
-  font-size: 22px;
-  
-  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMin}) {
-    font-size: 12px;
-  }
-  
-  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
-    font-size: 16px;
-  }
+    font-size: 22px;
+    
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMin}) {
+        font-size: 12px;
+    }
+    
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+        font-size: 16px;
+    }
 `;
 
 export const ButtonLink = styled(NavLink)`
-  font-size: 14px;
-  font-weight: bold;
-  line-height: 19px;
-  color: ${({ theme }) => theme.color.white};
-  background: ${({ theme }) => theme.color.blue};
-  text-decoration: none;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px 24px;
-  border-radius: 5px;
-  
-  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMin}) {
-    font-size: 10px;
-  }
+    font-size: 14px;
+    font-weight: bold;
+    line-height: 19px;
+    color: ${({ theme }) => theme.color.white};
+    background: ${({ theme }) => theme.color.blue};
+    text-decoration: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 5px;
 
-  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
-    font-size: 12px;
-  }
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMin}) {
+        font-size: 10px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+        font-size: 12px;
+    }
+
+    &:hover {
+        transform: scale(1.05);
+        filter: brightness(120%);
+        transition: all ease-out 0.5s;
+    }
+
+    &:active {
+        transform: scale(1);
+    }
 `;
--- src/common/LoadingPage/styled.js
+++ src/common/LoadingPage/styled.js
@@ -1,22 +1,23 @@
 import styled from "styled-components";
 
-export const Header = styled.header`
-    margin: 56px 10% 0px 10%;
+export const Header = styled.h2`
     font-weight: 600;
     font-size: 36px;
     line-height: 1.2;
-    color: ${({ theme }) => theme.color.woodsmoke};
-    
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        margin: 24px 5% 0px 5%;
-        font-weight: 500;
-        font-size: 18px;
-        line-height: 1.3;
+    margin-top: 64px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mediumScreen}) {
+        font-size: 30px;
+        margin-top: 50px;
     }
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMin}px) {
-        margin: 24px 5% 0px 5%;
-        font-weight: 500;
-        font-size: 14px;
-        line-height: 1.3;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+        font: 24px;
+        margin-top: 36px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.oldIphone}) {
+        font-size: 18px;
+        margin-top: 24px;
     }
 `;
