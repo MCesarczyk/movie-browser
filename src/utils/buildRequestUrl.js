305923326f6046f6fe79df5727f8a128bda348c0
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-23 09:27:42 +0200

    add encodeURIComponent function to handle reserved chars in query

--- src/utils/buildRequestUrl.js
+++ src/utils/buildRequestUrl.js
@@ -4,5 +4,5 @@ export const buildRequestUrl = (path, page, query) => {
     const apiLang = "&language=en-US";
     const apiAdult = "&include_adult=false";
 
-    return `${apiBaseUrl}${path}${apiKey}${page ? `&page=${page}` : ""}${apiLang}${query ? apiAdult : ""}${query ? `&query=${query}` : ""}`
+    return `${apiBaseUrl}${path}${apiKey}${page ? `&page=${page}` : ""}${apiLang}${query ? apiAdult : ""}${query ? `&query=${encodeURIComponent(query)}` : ""}`
 };
