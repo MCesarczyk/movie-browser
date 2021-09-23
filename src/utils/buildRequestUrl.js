b3dd7ba3273b1f0a23d44619ee248fb88930b9e6
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-23 10:52:25 +0200

    moved all actions to variables

--- src/utils/buildRequestUrl.js
+++ src/utils/buildRequestUrl.js
@@ -4,7 +4,9 @@ export const buildRequestUrl = (path, page, query) => {
     const apiBaseUrl = "https://api.themoviedb.org/3/";
     const apiKey = "?api_key=768f7875782193f5e4797762314da0b7";
     const apiLang = "&language=en-US";
-    const apiAdult = "&include_adult=false";
+    const apiAdult = query ? "&include_adult=false" : "";
+    const queryParam = query ? `&query=${fixedEncodeURIComponent(query)}` : "";
+    const pageParam = page ? `&page=${page}` : "";
 
-    return `${apiBaseUrl}${path}${apiKey}${page ? `&page=${page}` : ""}${apiLang}${query ? apiAdult : ""}${query ? `&query=${fixedEncodeURIComponent(query)}` : ""}`
+    return `${apiBaseUrl}${path}${apiKey}${apiLang}${apiAdult}${queryParam}${pageParam}`
 };
