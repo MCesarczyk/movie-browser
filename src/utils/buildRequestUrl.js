f5c80ed96a3e8beb3d2d63c0756ac837eefbec3e
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-23 09:32:54 +0200

    add function to handle some additional chars

--- src/utils/buildRequestUrl.js
+++ src/utils/buildRequestUrl.js
@@ -1,8 +1,10 @@
+import { fixedEncodeURIComponent } from "./fixedEncodeURIComponent";
+
 export const buildRequestUrl = (path, page, query) => {
     const apiBaseUrl = "https://api.themoviedb.org/3/";
     const apiKey = "?api_key=768f7875782193f5e4797762314da0b7";
     const apiLang = "&language=en-US";
     const apiAdult = "&include_adult=false";
 
-    return `${apiBaseUrl}${path}${apiKey}${page ? `&page=${page}` : ""}${apiLang}${query ? apiAdult : ""}${query ? `&query=${encodeURIComponent(query)}` : ""}`
+    return `${apiBaseUrl}${path}${apiKey}${page ? `&page=${page}` : ""}${apiLang}${query ? apiAdult : ""}${query ? `&query=${fixedEncodeURIComponent(query)}` : ""}`
 };
