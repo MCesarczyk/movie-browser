6d1259e196451a3c52e9445aead5ab30d2d14267
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-27 13:31:03 +0200

    noResultsPage description updated

--- src/core/CorePage/NoResultsPage/index.js
+++ src/core/CorePage/NoResultsPage/index.js
@@ -3,10 +3,10 @@ import { Wrapper } from "../../../common/Wrapper";
 import { NoResultsLogo } from "./styled";
 import noResults from "./noResults.svg";
 
-const NoResultsPage = () => (
+const NoResultsPage = ({ query }) => (
     <Wrapper>
         <Header>
-            {"Sorry, the page not found..."}
+            {`Sorry, there are no results for "${query}"`}
         </Header>
         {<NoResultsLogo src={noResults} alt="" />}
     </Wrapper>
