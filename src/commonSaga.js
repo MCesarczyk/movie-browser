f52174084625051fb927d8a9ac829bc05aeea8b9
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-09-27 06:01:49 +0200

    use buildRequestUrl in commonSaga

--- src/commonSaga.js
+++ src/commonSaga.js
@@ -1,14 +1,12 @@
 import { call, all, put, takeLatest } from "redux-saga/effects";
 import { getDataFromApi } from "./utils/getDataFromApi";
 import { fetchConfiguration, setConfiguration, setError, setMovieGenres } from "./commonSlice";
-
-const apiBaseUrl = "https://api.themoviedb.org/3/";
-const apiKey = "?api_key=768f7875782193f5e4797762314da0b7";
-const apiLang = "&language=en-US";
+import { buildRequestUrl } from "./utils/buildRequestUrl";
 
 function* fetchConfigurationHandler() {
     try {
-        const apiURL = `${apiBaseUrl}configuration${apiKey}${apiLang}`;
+        const path = "configuration";
+        const apiURL = buildRequestUrl(path);
         const configuration = yield call(getDataFromApi, apiURL);
         yield put(setConfiguration(configuration));
     } catch (error) {
@@ -18,7 +16,8 @@ function* fetchConfigurationHandler() {
 
 function* fetchMovieGenresHandler() {
     try {
-        const apiURL = `${apiBaseUrl}genre/movie/list${apiKey}${apiLang}`;
+        const path = "genre/movie/list";
+        const apiURL = buildRequestUrl(path);
         const genres = yield call(getDataFromApi, apiURL);
         yield put(setMovieGenres(genres.genres));
     } catch (error) {
