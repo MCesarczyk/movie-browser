2006a66336c285e9ffdc359af67354e5131ba315
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-03-18 03:09:10 +0100

    build: install @tanstack/react-query
    BREAKING CHANGE: changed react-query api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
